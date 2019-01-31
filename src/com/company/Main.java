f849d294c6adc2bb9b24c8e74a1be9eb954b04ac
jukegbu1 <jukegbu1@gmail.com>
2019-01-31 14:11:12 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Odd Numbers");
+        for(int i=1; i<=20; i++) {
+        if (i%2!= 0){
+        System.out.println(i + "");
+        }
+        }
+        } }
